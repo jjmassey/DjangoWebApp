0a658d050bcc97cb52e0007db3a9a307562f9a2b
jjmassey <joshua.fleet.1@uni.massey.ac.nz>
2018-09-05 12:12:45 +1200

    timezone update - working

--- webApp/webApp/settings.py
+++ webApp/webApp/settings.py
@@ -80,6 +80,7 @@ DATABASES = {
         'NAME': 'mysql',
         'USER': 'root',
         'PASSWORD': 'newpassword',
+
     }
 }
 
@@ -106,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'Pacific/Auckland'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Pacific/Auckland'
 
 USE_I18N = True
 
